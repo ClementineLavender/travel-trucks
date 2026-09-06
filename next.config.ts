d6d11c6c8d1815959490b02331f77c66e01910e4
ClementineLavender <natalibulavina78@gmail.com>
2026-09-06 17:00:18 +0200

    fix: allow camper image host

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "ac.goit.global" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "*.cloudinary.com" },
+      { protocol: "https", hostname: "*.amazonaws.com" },
+      { protocol: "https", hostname: "ftp.goit.study" },
+    ],
+  },
 };
 
 export default nextConfig;
